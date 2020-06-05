0f320558075f9dfc37d5d066296f240e56b01a81
Sean Downs <spdowns@protonmail.com>
2020-06-05 15:53:23 -0700

    adds fifth question, researching some jQuery effects

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 
   $("form#quiz").submit(function(event) {
     event.preventDefault();
+    $("#output").fadeIn();
 
     let favColor = $("input:radio[name=color]:checked").val();
     let favMovie = $("input:radio[name=movie]:checked").val();
@@ -12,31 +13,20 @@ $(document).ready(function() {
     if (favColor === 'yellow' && favMovie === 'hackers' && favBrand === 'nike' && favMusic === 'dark-ambient' || favColor === 'yellow') {
       hideResult(selection)
       $("#cobol").removeClass("hidden");
-      $("#output").fadeIn();
-      $("#output").fadeTo();
       selection = "#cobol"
     } else if (favColor === 'blue' && favMovie === 'sneakers' && favBrand === 'hanes' && favMusic === 'folktronica' || favColor === 'blue') {
       hideResult(selection)
       $("#csharp").removeClass("hidden");
-      $("#output").fadeIn();
-      $("#output").fadeTo();
       selection = "#csharp"
     } else if (favColor === 'other' && favMovie === 'daryl' && favBrand === 'apple' && favMusic === 'new-jack-mumble-swingcore' || favColor === 'other') {
       hideResult(selection)
       $("#basic").removeClass("hidden");
-      $("#output").fadeIn();
-      $("#output").fadeTo();
       selection = "#basic"
     } else if (favColor === 'red' && favMovie === 'the-net' || favColor === 'red') {
       hideResult(selection)
       $("#ruby").removeClass("hidden");
-      $("#output").fadeIn();
-      $("#output").fadeTo();
       selection = "#ruby"
     }
-    // $("#btn").click(function() {
-    //   $("#output").fadeIn();
-    // });
   });
 });
 

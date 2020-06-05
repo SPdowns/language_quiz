41c72da7002bce9315ff6e30fe9bf81879c10afe
Sean Downs <spdowns@protonmail.com>
2020-06-05 09:00:52 -0700

    show and hide the two available answers. going to read about  and plan some more questions and results

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,14 @@ $(document).ready(function() {
     event.preventDefault();
 
     let favColor = $("input:radio[name=color]:checked").val();
+
+    if (favColor === 'blue') {
+    $("#csharp").show();
+    $("#ruby").hide();
+    } else if (favColor === 'red') {
+      $("#csharp").hide();
+      $("#ruby").show();
+    }
+
   });
 });
